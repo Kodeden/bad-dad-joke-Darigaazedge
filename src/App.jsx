8ceecf94d2ce54a1218ce5a688aeeafb8d1b3dc8
Darigaazedge <105022024+Darigaazedge@users.noreply.github.com>
2023-08-14 17:04:09 -0700

    8-14-23
    Initial work started and most of the basics setup. Have buttons and the api get request is complete and working. Started in on the vote up and vote down buttons but they wont display for some reason.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,60 @@
+import { useState } from "react";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [randomDadJoke, setRandomDadJoke] = useState("");
+  const handleclick = async () => {
+    const url = "https://official-joke-api.appspot.com/random_joke";
+    const response = await fetch(url);
+    const joke = await response.json();
+    setRandomDadJoke(joke.setup + " " + joke.punchline);
+  };
+  const voteUp = () => {
+    console.log("vote up!");
+  };
+
+  const voteDown = () => {
+    console.log("vote Down!");
+  };
+
+  return (
+    <main>
+      <div>
+        <h1 className="flex justify-center text-3xl font-bold underline">
+          Dad Jokes!!
+        </h1>
+        ;
+        <h2 className="flex justify-center text-xl font-bold">
+          Vote up your favs and down vote the duds!!
+        </h2>
+      </div>
+
+      <p className="flex justify-center">Dad Joke: {randomDadJoke}</p>
+
+      <div className="flex justify-center">
+        <button
+          type="submit"
+          className="rounded-full bg-green-500 p-4 text-white"
+          onClick={handleclick}
+        >
+          Click for DadJoke
+        </button>
+      </div>
+      <div className="flex justify-around">
+        <div id="vote up box">
+          <button onClick={voteUp}>
+            <img src="Thumbs Up.png" alt="Thumbs Up" />
+          </button>
+          <ul></ul>
+        </div>
+        <div id="vote down box">
+          <button onClick={voteDown}>
+            <img src="Thumbs Down.png" alt="Thumbs Down" />
+          </button>
+          <ul></ul>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
